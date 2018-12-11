d2cdb5e53f2dc78e23c1907ff84aa272807c752e
Computer <computer@Computers-MacBook-Pro.local>
2018-12-11 15:29:52 -0700

    get even answered

--- homework2/app.js
+++ homework2/app.js
@@ -1,3 +1,11 @@
-for (var i = 0; i <= 200; i++) {
-    console.log(i);
-}
+//1 easy going
+
+//for (var i = 0; i <= 200; i++) {
+//  console.log(i);
+//}
+
+//1 get even
+
+//for (var i = 2; i <= 200; i = i + 2) {
+//  console.log(i);
+//}
